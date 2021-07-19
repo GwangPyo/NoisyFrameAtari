fad34d26fab17084ab67b353873f168067b6728a
Han ilseok <x2ever.han@gmail.com>
2021-07-19 22:07:24 +0900

    Update NoisyAtariWrapper

--- noisiy_atari/s.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import os
-
-os.system("unrar x Roms.rar")
-os.system("mkdir rars")
-os.system("mv HC\ROMS.zip   rars")
-os.system("mv ROMS.zip  rars")
-os.system("python -m atari_py.import_roms rars")
